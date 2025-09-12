ba536f11a1c7b16021870c52bb75528a5fc0e32a
jahur <jahurnur@gmail.com>
2025-09-13 09:48:41 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import UploadForm from "./UploadForm";
+import React from 'react';
+import UploadForm from './UploadForm';
+import './styles.css';
 
-function App() {
-  return <UploadForm />;
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-purple-200 to-blue-200">
+      <UploadForm />
+    </div>
+  );
 }
-
-export default App;
